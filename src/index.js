f708d97f2735505f03635b60bac74ece1e0662af
belladelange <isabella_delange@hotmail.com>
2023-09-03 16:57:48 +0100

    fix bug to show icon

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function displayWeatherCondition(response) {
 
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@nx.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
